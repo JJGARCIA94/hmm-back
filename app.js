2a759f9c5098e61ac14c4f28cc620b18136ade54
jonathan_escuela2 <jj.garcia.dl.1994@gmail.com>
2024-09-13 17:58:29 -0700

    remove ambulance

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const specialtiesRoutes = require('./app/routes/specialtiesRoutes');
 const hospitalsRoutes = require('./app/routes/hospitalsRoutes');
 const doctorsRoutes = require('./app/routes/doctorsRoutes');
-const requestAmbulanceRoutes = require('./app/routes/requestAmbulance');
 const cors = require('cors');
 const { PORT } = require("./config/const");
 const app = express();
@@ -18,7 +17,6 @@ app.use(cors());
 app.use('/specialties', specialtiesRoutes);
 app.use('/hospitals', hospitalsRoutes);
 app.use('/doctors', doctorsRoutes);
-app.use('/request-ambulance', requestAmbulanceRoutes);
 
 app.listen(PORT, () => {
     console.log('Server running on port '+PORT);
--- app/routes/requestAmbulance.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const requestAmbulanceController = require('../controllers/requestAmbulance');
-
-router.post('/', requestAmbulanceController.requestAmbulance);
-
-module.exports = router;
